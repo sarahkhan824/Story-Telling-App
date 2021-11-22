ecb29bdd3b96d3c5fbfcd8458ceec1c9a8089c53
sarahkhan824 <73019932+sarahkhan824@users.noreply.github.com>
2021-11-22 17:23:54 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAccfg8a-lUwO80iSdqbchPN_UN5zORI54",
+  authDomain: "storytellingapp-eab1a.firebaseapp.com",
+  projectId: "storytellingapp-eab1a",
+  storageBucket: "storytellingapp-eab1a.appspot.com",
+  messagingSenderId: "1070096540312",
+  appId: "1:1070096540312:web:a2d7a0686c4b2edba1e6ed"
+};
+
